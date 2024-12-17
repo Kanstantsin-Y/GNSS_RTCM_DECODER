0c69146f74a99b56ac08b436ff2dec6b1c2f205d
Kanstantsin-Y <konstantin.yuriev83@gmail.com>
2024-12-17 19:51:43 +0200

    Grooming. Fixed linter warnings in gnss_types.

--- gnss_types/data_class_methods.py
+++ gnss_types/data_class_methods.py
@@ -5,18 +5,26 @@
     This file implements some usefull methods for gnss dataclass objects.
 """
 
+# pylint: disable = invalid-name
+
 import math
 import copy
-from dataclasses import dataclass, fields, is_dataclass, asdict
+
+from typing import Any
+from itertools import zip_longest
+from dataclasses import dataclass, fields, is_dataclass
 from collections.abc import Iterable
 
 
 DEF_FLOAT_CMP_TOLR = 1e-15
 
+
 @dataclass
 class DataClassMethods:
-    
+    """Provides some methods to be used with gnss data types (dataclasses)."""
+
     def items(self):
+        """Iterator over fields"""
         for field in fields(self.__class__):
             yield field.name, getattr(self, field.name)
 
@@ -24,88 +32,89 @@ class DataClassMethods:
         """Shallow copy of the object"""
         ret = self.__class__()
         for key, value in self.items():
-            setattr(ret,key,value)
+            setattr(ret, key, value)
 
         return ret
-    
+
     def deepcopy(self):
         """Return deep copy of the object"""
         return copy.deepcopy(self)
-    
-    def len(self):
-        return len(fields(self))
-    
-    
-    @classmethod
-    def __compare(cls, A, B, f_tolerance:float) -> bool:
-        """ Compare 'A' versus 'B'.
-            Integer and string fields shall match absolutely.
-            Floats are compared with predefined relative tolerance.
-            Comparison is done in 'deep' manner - goes recursively 
-            through compound elements untll decomposed to primitives. 
-            """
 
-        if type(A) != type(B):
+    def len(self):
+        """Get the number of fields"""
+        return len(fields(self))
+
+    @classmethod
+    def __compare(cls, A: Any, B: Any, f_tolerance: float) -> bool:
+        """Compare 'A' versus 'B'.
+        Integer and string fields shall match absolutely.
+        Floats are compared with predefined relative tolerance.
+        Comparison is done in 'deep' manner - goes recursively
+        through compound elements untll decomposed to primitives.
+        """
+
+        if not isinstance(A, type(B)):
             return False
-        
+
         if is_dataclass(A):
-            A = asdict(A)
-            B = asdict(B)
-        
+            return False
+            # A = asdict(A)
+            # B = asdict(B)
+
         if isinstance(A, (int, str, bool)):
-            return (A == B)
-        
-        elif isinstance(A, float):
+            return A == B
+
+        if isinstance(A, float):
             if math.fabs(B > f_tolerance):
-                diff = math.fabs((B-A)/B)
+                diff = math.fabs((B - A) / B)
             else:
-                diff = math.fabs(B-A)
-            return (diff < f_tolerance)
-                    
-        elif isinstance(A, dict):
+                diff = math.fabs(B - A)
+
+            return diff < f_tolerance
+
+        if isinstance(A, dict):
 
             if A.keys() != B.keys():
                 return False
-            
+
             for key in A:
-                if not cls.__compare(A[key],B[key],f_tolerance):
+                if not cls.__compare(A[key], B[key], f_tolerance):
                     return False
 
         elif isinstance(A, Iterable):
-            
-            if len(A) != len(B):
-                return False
 
-            for a, b in zip(A, B):
-                if not cls.__compare(a,b,f_tolerance):
+            for a, b in zip_longest(A, B, fillvalue=None):
+                if not cls.__compare(a, b, f_tolerance):
                     return False
-    
-        else:
-            raise TypeError(f'Unexpected type {type(A)}. Comparison is impossible.')
-        
-        return True
-       
-    
-    def compare(self, refs, f_tolerance:float = DEF_FLOAT_CMP_TOLR) -> bool:
-        """ Compare versus 'refs' field by field.
-            Integer and string fields shall match absolutely.
-            Floats are compared with predefined relative tolerance.
-            Compaund fields are checked recursively (deep comparison)."""
 
-        if type(self) != type(refs):
+        else:
+            raise TypeError(f"Unexpected type {type(A)}. Comparison is impossible.")
+
+        return True
+
+    def compare(self, refs, f_tolerance: float = DEF_FLOAT_CMP_TOLR) -> bool:
+        """Compare versus 'refs' field by field.
+        Integer and string fields shall match absolutely.
+        Floats are compared with predefined relative tolerance.
+        Compaund fields are checked recursively (deep comparison)."""
+
+        if not isinstance(self, type(refs)):
             return False
-        
+
         for key, value in self.items():
             try:
-                ref = getattr(refs,key)
+                ref = getattr(refs, key)
             except IndexError:
                 return False
-            
+
             if is_dataclass(value):
-                eq = value.compare(ref)
+                if isinstance(value, DataClassMethods):
+                    eq = value.compare(ref)
+                else:
+                    eq = False
             else:
-                eq = self.__compare(value,ref,f_tolerance)
-            
+                eq = self.__compare(value, ref, f_tolerance)
+
             if not eq:
                 return False
 
--- gnss_types/ephemeris.py
+++ gnss_types/ephemeris.py
@@ -5,126 +5,134 @@
     This file implements DTO classes for ephemerida data.
 """
 
+# pylint: disable = invalid-name, too-many-instance-attributes
+
 from dataclasses import dataclass
 from .data_class_methods import DataClassMethods
 
-__all__ = ['EphGPS', 'EphGLO', 'EphGALF', 'EphGALI', 'EphBDS', 'EphQZS', 'EphNAVIC']
+__all__ = ["EphGPS", "EphGLO", "EphGALF", "EphGALI", "EphBDS", "EphQZS", "EphNAVIC"]
 
 
 @dataclass
 class EphHdr:
     """ephemeris ID"""
-    #3 elements:
+
+    # 3 elements:
     msgNum: int = 0
     satNum: int = 0
     weekNum: int = 0
 
+
 @dataclass
 class ClockBias:
     """Clock correction parameters"""
-    #4 elements:
+
+    # 4 elements:
     tc: int = 0
-    af0: int|float = 0
-    af1: int|float = 0
-    af2: int|float = 0
+    af0: int | float = 0
+    af1: int | float = 0
+    af2: int | float = 0
+
 
 @dataclass
 class Keplerians:
     """SV's orbital motion parameters"""
+
     # 16 elements:
-    te:         int = 0
-    delta_n:    int|float = 0
-    m0:         int|float = 0
-    e:          int|float = 0
-    sqrt_a:     int|float = 0
-    w:          int|float = 0
-    i0:         int|float = 0
-    i_dot:      int|float = 0
-    omega0:     int|float = 0
-    omega_dot:  int|float = 0
-    crs:        int|float = 0
-    crc:        int|float = 0
-    cuc:        int|float = 0
-    cus:        int|float = 0
-    cic:        int|float = 0
-    cis:        int|float = 0
-    
+    te: int = 0
+    delta_n: int | float = 0
+    m0: int | float = 0
+    e: int | float = 0
+    sqrt_a: int | float = 0
+    w: int | float = 0
+    i0: int | float = 0
+    i_dot: int | float = 0
+    omega0: int | float = 0
+    omega_dot: int | float = 0
+    crs: int | float = 0
+    crc: int | float = 0
+    cuc: int | float = 0
+    cus: int | float = 0
+    cic: int | float = 0
+    cis: int | float = 0
+
 
 @dataclass
 class EphGALF(DataClassMethods, Keplerians, ClockBias, EphHdr):
-    """ Galileo FNAV ephemeris, MSG 1045"""
+    """Galileo FNAV ephemeris, MSG 1045"""
+
     # 23 + 5 = 28 elements
     IODnav: int = 0
-    SISA: int|float = 0
-    E5a_BGD: int|float = 0
+    SISA: int | float = 0
+    E5a_BGD: int | float = 0
     E5a_SHS: int = 0
     E5a_DVS: int = 0
 
 
 @dataclass
 class EphGALI(DataClassMethods, Keplerians, ClockBias, EphHdr):
-    """ Galileo INAV ephemeris, MSG 1046"""
+    """Galileo INAV ephemeris, MSG 1046"""
 
-    #23 + 8 = 31 elements
+    # 23 + 8 = 31 elements
     IODnav: int = 0
-    SISA: int|float = 0
-    E5a_BGD: int|float = 0
-    E5b_BGD: int|float = 0
+    SISA: int | float = 0
+    E5a_BGD: int | float = 0
+    E5b_BGD: int | float = 0
     E1_SHS: int = 0
     E1_DVS: int = 0
     E5b_SHS: int = 0
     E5b_DVS: int = 0
 
-      
+
 @dataclass
 class EphQZS(DataClassMethods, Keplerians, ClockBias, EphHdr):
-    """ QZSS L1 ephemeris, MSG 1044"""
+    """QZSS L1 ephemeris, MSG 1044"""
 
-    #23 + 7 = 30 elements
+    # 23 + 7 = 30 elements
     IODE: int = 0
     IODC: int = 0
     Fit: int = 0
     L2_Codes: int = 0
     SVH: int = 0
-    URA: int|float = 0
-    TGD: int|float = 0
+    URA: int | float = 0
+    TGD: int | float = 0
 
 
 @dataclass
 class EphBDS(DataClassMethods, Keplerians, ClockBias, EphHdr):
-    """ BeiDou D1 ephemeris, MSG 1042"""
+    """BeiDou D1 ephemeris, MSG 1042"""
 
-    #23 + 6 = 29 elements
+    # 23 + 6 = 29 elements
     AODE: int = 0
     AODC: int = 0
     SVH: int = 0
-    URAI: int|float = 0
-    TGD1: int|float = 0
-    TGD2: int|float = 0
+    URAI: int | float = 0
+    TGD1: int | float = 0
+    TGD2: int | float = 0
 
 
 @dataclass
 class EphGPS(DataClassMethods, Keplerians, ClockBias, EphHdr):
-    """ GPS LNAV/CNAV ephemeris, MSG 1019"""
+    """GPS LNAV/CNAV ephemeris, MSG 1019"""
 
-    #23 + 8 = 31 elements
+    # 23 + 8 = 31 elements
     IODE: int = 0
     IODC: int = 0
-    Fit: int|str = 0
-    L2_Codes: int|str = 0
-    L2P_Data: int|str = 0
-    SVH: int|str = 0
-    URA: int|float = 0
-    TGD: int|float = 0
+    Fit: int | str = 0
+    L2_Codes: int | str = 0
+    L2P_Data: int | str = 0
+    SVH: int | str = 0
+    URA: int | float = 0
+    TGD: int | float = 0
 
 
 @dataclass
 class EphNAVIC(DataClassMethods, Keplerians, ClockBias, EphHdr):
-    """ Navic L5/S ephemeris, MSG 1041"""
+    """Navic L5/S ephemeris, MSG 1041"""
 
-    #23 + 5 = 28 elements
-    URA: int|float = 0
-    TGD: int|float = 0
+    # 23 + 5 = 28 elements
+    URA: int | float = 0
+    TGD: int | float = 0
     IODEC: int = 0
     L5_Flag = 0
     S_Flag = 0
@@ -134,40 +142,40 @@ class EphNAVIC(DataClassMethods, Keplerians, ClockBias, EphHdr):
 class EphGLO(DataClassMethods):
     """Glonass L1/L2 ephemeris, MSG 1020"""
 
-    #36 items
-    msgNum:         int = 0
-    satNum:         int = 0
-    frqSloNum:      int = 0         # [-7..6]
-    tk:             int = 0         # [sec]
-    tb:             int = 0         # [sec]
-    xn:             int|float = 0   # [km/s]
-    dotXn:          int|float = 0   # [km/s^2]
-    dotDotXn:       int|float = 0   # [km/s^3]
-    yn:             int|float = 0   # [km/s]
-    dotYn:          int|float = 0   # [km/s^2]
-    dotDotYn:       int|float = 0   # [km/s^3]
-    zn:             int|float = 0   # [km/s]
-    dotZn:          int|float = 0   # [km/s^2]
-    dotDotZn:       int|float = 0   # [km/s^3]
-    gamma_n:        int|float = 0   # []
-    Cn:             int = 0
-    BnMSB:          int = 0
-    AlmHAI:         int = 0
-    P:              int = 0
-    P1:             int = 0         # [sec]
-    P2:             int = 0
-    P3:             int = 0
-    P4:             int = 0
-    ln3:            int = 0
-    tauN:           int|float = 0   # [m]
-    delta_tauN:     int|float = 0   # [m]
-    En:             int = 0
-    Ft:             int = 0         # [m]
-    Nt:             int = 0
-    M:              int = 0
-    auxDataOK:      int = 0
-    Na:             int = 0
-    tauC:           int|float = 0   # [s]
-    N4:             int = 0
-    tauGPS:         int|float = 0   # [s]
-    ln5:            int = 0
+    # 36 items
+    msgNum: int = 0
+    satNum: int = 0
+    frqSloNum: int = 0  # [-7..6]
+    tk: int = 0  # [sec]
+    tb: int = 0  # [sec]
+    xn: int | float = 0  # [km/s]
+    dotXn: int | float = 0  # [km/s^2]
+    dotDotXn: int | float = 0  # [km/s^3]
+    yn: int | float = 0  # [km/s]
+    dotYn: int | float = 0  # [km/s^2]
+    dotDotYn: int | float = 0  # [km/s^3]
+    zn: int | float = 0  # [km/s]
+    dotZn: int | float = 0  # [km/s^2]
+    dotDotZn: int | float = 0  # [km/s^3]
+    gamma_n: int | float = 0  # []
+    Cn: int = 0
+    BnMSB: int = 0
+    AlmHAI: int = 0
+    P: int = 0
+    P1: int = 0  # [sec]
+    P2: int = 0
+    P3: int = 0
+    P4: int = 0
+    ln3: int = 0
+    tauN: int | float = 0  # [m]
+    delta_tauN: int | float = 0  # [m]
+    En: int = 0
+    Ft: int = 0  # [m]
+    Nt: int = 0
+    M: int = 0
+    auxDataOK: int = 0
+    Na: int = 0
+    tauC: int | float = 0  # [s]
+    N4: int = 0
+    tauGPS: int | float = 0  # [s]
+    ln5: int = 0
